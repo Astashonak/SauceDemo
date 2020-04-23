74551ec6d07b013c7f8c753def208cb8e8585688
e.astashonak <e.anischyk@gmail.com>
2020-04-23 20:14:10 +0300

    added "practice Lesson 9"

---
+++ src/test/java/pages/ProductsPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProductsPage extends BasePage {
+
+    private static final String ADD_TO_CART_LOCATOR = "//*[text()='%s']/../../..//button";
+
+    private static final String URL = "https://www.saucedemo.com/inventory.html";
+
+    public ProductsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void openPage() {
+        driver.get(URL);
+    }
+
+    public void addToCart(String productName){
+        By addToCartXpath = By.xpath(String.format(ADD_TO_CART_LOCATOR, productName));
+        driver.findElement(addToCartXpath).click();
+    }
+
+
+}
